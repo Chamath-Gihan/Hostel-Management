26a6f1dbb4c80f3bfa09dd595980ce8e3c6d149e
Chamath-Gihan <spchandanipushpakumari@gmail.com>
2023-09-10 02:05:40 +0530

    Custom DTO

--- src/lk/ijse/hostel_management_system/dto/CustomDTO.java
+++ src/lk/ijse/hostel_management_system/dto/CustomDTO.java
@@ -1,4 +1,35 @@
 package lk.ijse.hostel_management_system.dto;
 
+import lk.ijse.hostel_management_system.entity.Reservation;
+import lk.ijse.hostel_management_system.entity.Room;
+import lk.ijse.hostel_management_system.entity.Student;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CustomDTO {
+
+    // reservation
+    private String res_id;
+    private LocalDate res_date;
+    private String status;
+
+    private Student student;
+    private Room room;
+
+    // Room
+    private String room_type_id;
+    private String type;
+    private String key_money;
+    private int qty;
+    private List<Reservation> reservationList = new ArrayList<>();
+
+    // Student
+    private String id;
+    private String name;
+    private String address;
+    private String contact_no;
+    private String dob;
+    private String gender;
 }
