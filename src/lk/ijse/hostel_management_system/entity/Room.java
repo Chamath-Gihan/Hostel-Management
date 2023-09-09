6f26b4bc301626eca8cb2197ca94dc3cb757305d
Chamath-Gihan <spchandanipushpakumari@gmail.com>
2023-09-10 01:57:19 +0530

    Room Entity Class

--- src/lk/ijse/hostel_management_system/entity/Room.java
+++ src/lk/ijse/hostel_management_system/entity/Room.java
@@ -19,5 +19,65 @@ public class Room {
     @OneToMany(mappedBy = "room" ,cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<Reservation> reservationList = new ArrayList<>();
 
-    
+    @Override
+    public String toString() {
+        return "Room{" +
+                "room_type_id='" + room_type_id + '\'' +
+                ", type='" + type + '\'' +
+                ", key_money='" + key_money + '\'' +
+                ", qty=" + qty +
+                ", reservationList=" + reservationList +
+                '}';
+    }
+
+    public String getRoom_type_id() {
+        return room_type_id;
+    }
+
+    public void setRoom_type_id(String room_type_id) {
+        this.room_type_id = room_type_id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getKey_money() {
+        return key_money;
+    }
+
+    public void setKey_money(String key_money) {
+        this.key_money = key_money;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    public List<Reservation> getReservationList() {
+        return reservationList;
+    }
+
+    public void setReservationList(List<Reservation> reservationList) {
+        this.reservationList = reservationList;
+    }
+
+    public Room() {
+    }
+
+    public Room(String room_type_id, String type, String key_money, int qty, List<Reservation> reservationList) {
+        this.room_type_id = room_type_id;
+        this.type = type;
+        this.key_money = key_money;
+        this.qty = qty;
+        this.reservationList = reservationList;
+    }
 }
