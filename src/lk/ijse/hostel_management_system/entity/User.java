438307e473fb5e3eaebcd87ea57eb963f84de4ac
Chamath-Gihan <spchandanipushpakumari@gmail.com>
2023-09-10 01:58:01 +0530

    User Entity Class

--- src/lk/ijse/hostel_management_system/entity/User.java
+++ src/lk/ijse/hostel_management_system/entity/User.java
@@ -1,4 +1,15 @@
 package lk.ijse.hostel_management_system.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class User {
+
+    @Id
+    private String id;
+
+    private String name;
+
+    private String password;
 }
