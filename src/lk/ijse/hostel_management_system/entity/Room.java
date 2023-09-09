ae8b0e47ec5f080d8dac5f5e325ab77766fb75cf
Chamath-Gihan <spchandanipushpakumari@gmail.com>
2023-09-10 01:56:52 +0530

    Room Entity Class

--- src/lk/ijse/hostel_management_system/entity/Room.java
+++ src/lk/ijse/hostel_management_system/entity/Room.java
@@ -1,4 +1,23 @@
 package lk.ijse.hostel_management_system.entity;
 
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Room {
+
+    @Id
+    private String room_type_id;
+
+    private String type;
+
+    private String key_money;
+
+    private int qty;
+
+    @OneToMany(mappedBy = "room" ,cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Reservation> reservationList = new ArrayList<>();
+
+    
 }
