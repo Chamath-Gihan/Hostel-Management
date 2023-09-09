579a073c347baf5f3702c5b871d0e9aaffae9e09
Chamath-Gihan <spchandanipushpakumari@gmail.com>
2023-09-10 01:55:27 +0530

    Reservation Entity Class

--- src/lk/ijse/hostel_management_system/entity/Reservation.java
+++ src/lk/ijse/hostel_management_system/entity/Reservation.java
@@ -1,4 +1,23 @@
 package lk.ijse.hostel_management_system.entity;
 
+import javax.persistence.*;
+
+@Entity
 public class Reservation {
+
+    @Id
+    private String res_id;
+
+    @Column(columnDefinition = "DATE")
+    private String res_date;
+
+    private String status;
+
+    @ManyToOne
+    @JoinColumn(name = "student_id")
+    private Student student;
+
+    @ManyToOne
+    @JoinColumn(name = "room_type_id")
+    private Room room;
 }
