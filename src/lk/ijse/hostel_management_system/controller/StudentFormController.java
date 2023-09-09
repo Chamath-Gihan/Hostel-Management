41bad0dcfec0c6c33ab29905dd9a69732731a2d3
Chamath-Gihan <spchandanipushpakumari@gmail.com>
2023-09-10 00:27:00 +0530

    Student form created

--- src/lk/ijse/hostel_management_system/controller/StudentFormController.java
+++ src/lk/ijse/hostel_management_system/controller/StudentFormController.java
@@ -1,4 +1,97 @@
 package lk.ijse.hostel_management_system.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
 public class StudentFormController {
+
+    @FXML
+    private ImageView btnBack;
+
+    @FXML
+    private Button btnDelete;
+
+    @FXML
+    private Button btnNew;
+
+    @FXML
+    private Button btnSave;
+
+    @FXML
+    private Button btnUpdate;
+
+    @FXML
+    private ComboBox<?> cmbGender;
+
+    @FXML
+    private TableColumn<?, ?> colAddress;
+
+    @FXML
+    private TableColumn<?, ?> colContact;
+
+    @FXML
+    private TableColumn<?, ?> colDateOfBirth;
+
+    @FXML
+    private TableColumn<?, ?> colGender;
+
+    @FXML
+    private TableColumn<?, ?> colID;
+
+    @FXML
+    private TableColumn<?, ?> colName;
+
+    @FXML
+    private ImageView root;
+
+    @FXML
+    private TableView<?> tblStudent;
+
+    @FXML
+    private TextField txtAddress;
+
+    @FXML
+    private TextField txtContact;
+
+    @FXML
+    private TextField txtDateOfBirth;
+
+    @FXML
+    private TextField txtID;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    void btnBackOnMouseClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void btnDeleteOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnNewOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSaveOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUpdateOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void cmbGenderOnAction(ActionEvent event) {
+
+    }
 }
