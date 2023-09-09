054fa2b70dc9ee2a2ceff4d1c7eb72bb8529792a
Chamath-Gihan <spchandanipushpakumari@gmail.com>
2023-09-10 00:59:43 +0530

    room form created

--- src/lk/ijse/hostel_management_system/controller/RoomFormController.java
+++ src/lk/ijse/hostel_management_system/controller/RoomFormController.java
@@ -1,4 +1,70 @@
 package lk.ijse.hostel_management_system.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
 public class RoomFormController {
+
+    @FXML
+    private ImageView btnBack;
+
+    @FXML
+    private Button btnNew;
+
+    @FXML
+    private Button btnSave;
+
+    @FXML
+    private Button btnUpdate;
+
+    @FXML
+    private ComboBox<?> cmbType;
+
+    @FXML
+    private TableColumn<?, ?> colID;
+
+    @FXML
+    private TableColumn<?, ?> colKeyMoney;
+
+    @FXML
+    private TableColumn<?, ?> colQTY;
+
+    @FXML
+    private TableColumn<?, ?> colType;
+
+    @FXML
+    private TableView<?> tblStudent;
+
+    @FXML
+    private TextField txtID;
+
+    @FXML
+    private TextField txtKeyMoney;
+
+    @FXML
+    private TextField txtQty;
+
+    @FXML
+    void btnBackOnMouseClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void btnNewOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSaveOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUpdateOnAction(ActionEvent event) {
+
+    }
+
 }
