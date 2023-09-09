155fc7d2bb8b7612b4aa9437b57ff0805ec61eba
Chamath-Gihan <spchandanipushpakumari@gmail.com>
2023-09-09 23:40:49 +0530

    Login form created

--- src/lk/ijse/hostel_management_system/controller/LoginFormController.java
+++ src/lk/ijse/hostel_management_system/controller/LoginFormController.java
@@ -1,4 +1,24 @@
 package lk.ijse.hostel_management_system.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class LoginFormController {
+
+    @FXML
+    private Button btnLogin;
+
+    @FXML
+    private TextField txtPassword;
+
+    @FXML
+    private TextField txtUserName;
+
+    @FXML
+    void btnLoginOnAction(ActionEvent event) {
+
+    }
+
 }
