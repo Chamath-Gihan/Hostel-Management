3d6ab189969f7c7ad72adf30fbc88d3215e36cc1
Chamath-Gihan <spchandanipushpakumari@gmail.com>
2023-09-10 01:36:29 +0530

    changes

--- src/lk/ijse/hostel_management_system/controller/SettingsFormController.java
+++ src/lk/ijse/hostel_management_system/controller/SettingsFormController.java
@@ -1,4 +1,36 @@
 package lk.ijse.hostel_management_system.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
 public class SettingsFormController {
+
+    @FXML
+    private ImageView btnBack;
+
+    @FXML
+    private Button btnSave;
+
+    @FXML
+    private ImageView root;
+
+    @FXML
+    private TextField txtPassword;
+
+    @FXML
+    private TextField txtUserName;
+
+    @FXML
+    void btnBackOnMouseClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void btnSaveOnAction(ActionEvent event) {
+
+    }
 }
