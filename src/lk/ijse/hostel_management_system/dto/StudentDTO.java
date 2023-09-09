eeb1071c7e06314983a55b1316e86074b1d9f5df
Chamath-Gihan <spchandanipushpakumari@gmail.com>
2023-09-10 02:09:00 +0530

    Student DTO

--- src/lk/ijse/hostel_management_system/dto/StudentDTO.java
+++ src/lk/ijse/hostel_management_system/dto/StudentDTO.java
@@ -1,4 +1,83 @@
 package lk.ijse.hostel_management_system.dto;
 
 public class StudentDTO {
+
+    private String id;
+    private String name;
+    private String address;
+    private String contact_no;
+    private String dob;
+    private String gender;
+
+    @Override
+    public String toString() {
+        return "StudentDTO{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", contact_no='" + contact_no + '\'' +
+                ", dob='" + dob + '\'' +
+                ", gender='" + gender + '\'' +
+                '}';
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getContact_no() {
+        return contact_no;
+    }
+
+    public void setContact_no(String contact_no) {
+        this.contact_no = contact_no;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public StudentDTO() {
+    }
+
+    public StudentDTO(String id, String name, String address, String contact_no, String dob, String gender) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.contact_no = contact_no;
+        this.dob = dob;
+        this.gender = gender;
+    }
 }
