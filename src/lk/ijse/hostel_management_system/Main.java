447ba3279e8a736c6730c6eea15b67e4d2d79b26
Chamath-Gihan <spchandanipushpakumari@gmail.com>
2023-09-10 02:26:20 +0530

    Login

--- src/lk/ijse/hostel_management_system/Main.java
+++ src/lk/ijse/hostel_management_system/Main.java
@@ -1,6 +1,8 @@
 package lk.ijse.hostel_management_system;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -10,6 +12,10 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-
+        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/resources/view/login_form.fxml"))));
+        primaryStage.show();
+        primaryStage.setResizable(false);
+        primaryStage.setY(0);
+        primaryStage.setTitle("24D Hostel Management System | Login");
     }
 }
--- src/lk/ijse/hostel_management_system/controller/LoginFormController.java
+++ src/lk/ijse/hostel_management_system/controller/LoginFormController.java
@@ -1,12 +1,19 @@
 package lk.ijse.hostel_management_system.controller;
 
+import animatefx.animation.FadeIn;
+import animatefx.animation.Shake;
+import javafx.beans.binding.Bindings;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Paint;
 
 public class LoginFormController {
+
+    UserBO userBO =(UserBO) BOFactory.getBoFactory().getBO(BOFactory.Type.USER);
 
     @FXML
     private Button btnLogin;
@@ -21,8 +28,73 @@ public class LoginFormController {
     private AnchorPane root;
 
     @FXML
-    void btnLoginOnAction(ActionEvent event) {
+    private ToggleButton toggleButton;
 
+    @FXML
+    private Label lblShownPassword;
+
+    public void initialize(){
+        lblShownPassword.setVisible(false);
     }
 
+    @FXML
+    void btnLoginOnAction(ActionEvent event) {
+        Shake shakeUserName = new Shake(txtUserName);
+        Shake shakePassword = new Shake(txtPassword);
+
+        if( isCorrectPassword() && isCorrectUserName()){
+            txtUserName.setFocusColor(Paint.valueOf("BLUE"));
+            Navigation.navigate(Routes.DASHBOARD, pane);
+            new FadeIn(pane).setSpeed(3).play();
+
+        }else if (isCorrectPassword() && !isCorrectUserName()) {
+            txtUserName.requestFocus();
+            txtUserName.setFocusColor(Paint.valueOf("RED"));
+            shakeUserName.play();
+        } else if (!isCorrectPassword() && isCorrectUserName()) {
+            txtPassword.requestFocus();
+            txtPassword.setFocusColor(Paint.valueOf("RED"));
+            shakePassword.play();
+        } else{
+            new Alert(Alert.AlertType.ERROR,"Try again !").show();
+            txtPassword.clear();
+            txtUserName.clear();
+        }
+    }
+
+    @FXML
+    void txtPasswordOnKeyPressed(KeyEvent event) {
+        lblShownPassword.textProperty().bind(Bindings.concat(txtPassword.getText()));
+    }
+
+    public void toggleButton(ActionEvent actionEvent) {
+        if (toggleButton.isSelected()) {
+            lblShownPassword.setVisible(true);
+            lblShownPassword.textProperty().bind(Bindings.concat(txtPassword.getText()));
+            toggleButton.setText("Hide");
+
+        }else{
+            lblShownPassword.setVisible(false);
+            txtPassword.setVisible(true);
+            toggleButton.setText("Show");
+        }
+    }
+
+    private boolean isCorrectUserName() {
+        String user = userBO.getUser("1");
+        if(user == null){
+            new Alert(Alert.AlertType.ERROR," Database Error !").show();
+            return false;
+        }
+        return txtUserName.getText().equals(user);
+    }
+
+    private boolean isCorrectPassword() {
+        String password = userBO.getPassword("1");
+        if(password == null){
+            new Alert(Alert.AlertType.ERROR," Database Error !").show();
+            return false;
+        }
+        return txtPassword.getText().equals(password);
+    }
 }
