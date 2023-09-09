116efff9f4cbaedadf6eac2330524134eee21e69
Chamath-Gihan <spchandanipushpakumari@gmail.com>
2023-09-10 01:50:13 +0530

    Student Entity Class

--- src/lk/ijse/hostel_management_system/entity/Student.java
+++ src/lk/ijse/hostel_management_system/entity/Student.java
@@ -1,4 +1,23 @@
 package lk.ijse.hostel_management_system.entity;
 
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Student {
+    @Id
+    private String id;
+    private String name;
+    @Column(columnDefinition = "TEXT")
+    private String address;
+    private String contact_no;
+    @Column(columnDefinition = "DATE")
+    private String dob;
+    private String gender;
+
+    @OneToMany(mappedBy = "student" ,cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Reservation> reservationList = new ArrayList<>();
+    
+
 }
