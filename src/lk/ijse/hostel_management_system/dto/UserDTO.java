2acb93a60e169a9176c92be2aabea99832265919
Chamath-Gihan <spchandanipushpakumari@gmail.com>
2023-09-10 02:09:50 +0530

    User DTO

--- src/lk/ijse/hostel_management_system/dto/UserDTO.java
+++ src/lk/ijse/hostel_management_system/dto/UserDTO.java
@@ -1,4 +1,50 @@
 package lk.ijse.hostel_management_system.dto;
 
 public class UserDTO {
+
+    private String id;
+    private String name;
+    private String password;
+
+    @Override
+    public String toString() {
+        return "UserDTO{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public UserDTO() {
+    }
+
+    public UserDTO(String id, String name, String password) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+    }
 }
