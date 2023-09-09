7ee1f776c5be8afb976a24a0e94bf352bfed7725
Chamath-Gihan <spchandanipushpakumari@gmail.com>
2023-09-10 01:55:55 +0530

    Reservation Entity Class

--- src/lk/ijse/hostel_management_system/entity/Reservation.java
+++ src/lk/ijse/hostel_management_system/entity/Reservation.java
@@ -20,4 +20,66 @@ public class Reservation {
     @ManyToOne
     @JoinColumn(name = "room_type_id")
     private Room room;
+
+    @Override
+    public String toString() {
+        return "Reservation{" +
+                "res_id='" + res_id + '\'' +
+                ", res_date='" + res_date + '\'' +
+                ", status='" + status + '\'' +
+                ", student=" + student +
+                ", room=" + room +
+                '}';
+    }
+
+    public String getRes_id() {
+        return res_id;
+    }
+
+    public void setRes_id(String res_id) {
+        this.res_id = res_id;
+    }
+
+    public String getRes_date() {
+        return res_date;
+    }
+
+    public void setRes_date(String res_date) {
+        this.res_date = res_date;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public Room getRoom() {
+        return room;
+    }
+
+    public void setRoom(Room room) {
+        this.room = room;
+    }
+
+    public Reservation() {
+    }
+
+    public Reservation(String res_id, String res_date, String status, Student student, Room room) {
+        this.res_id = res_id;
+        this.res_date = res_date;
+        this.status = status;
+        this.student = student;
+        this.room = room;
+    }
 }
