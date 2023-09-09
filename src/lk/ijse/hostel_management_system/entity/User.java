c0d043080df3d8211d3c315666bcc2cf6141ce6c
Chamath-Gihan <spchandanipushpakumari@gmail.com>
2023-09-10 01:58:46 +0530

    User Entity Class

--- src/lk/ijse/hostel_management_system/entity/User.java
+++ src/lk/ijse/hostel_management_system/entity/User.java
@@ -12,4 +12,46 @@ public class User {
     private String name;
 
     private String password;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public User() {
+    }
+
+    public User(String id, String name, String password) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+    }
 }
