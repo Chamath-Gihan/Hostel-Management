3e7230a15ea28e28657c783ac35cb54f5c465ab8
Chamath-Gihan <spchandanipushpakumari@gmail.com>
2023-09-10 02:08:05 +0530

    Rooms DTO

--- src/lk/ijse/hostel_management_system/dto/RoomsDTO.java
+++ src/lk/ijse/hostel_management_system/dto/RoomsDTO.java
@@ -1,4 +1,61 @@
 package lk.ijse.hostel_management_system.dto;
 
 public class RoomsDTO {
+
+    private String room_type_id;
+    private String type;
+    private String key_money;
+    private int qty;
+
+    @Override
+    public String toString() {
+        return "RoomsDTO{" +
+                "room_type_id='" + room_type_id + '\'' +
+                ", type='" + type + '\'' +
+                ", key_money='" + key_money + '\'' +
+                ", qty=" + qty +
+                '}';
+    }
+
+    public String getRoom_type_id() {
+        return room_type_id;
+    }
+
+    public void setRoom_type_id(String room_type_id) {
+        this.room_type_id = room_type_id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getKey_money() {
+        return key_money;
+    }
+
+    public void setKey_money(String key_money) {
+        this.key_money = key_money;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    public RoomsDTO() {
+    }
+
+    public RoomsDTO(String room_type_id, String type, String key_money, int qty) {
+        this.room_type_id = room_type_id;
+        this.type = type;
+        this.key_money = key_money;
+        this.qty = qty;
+    }
 }
